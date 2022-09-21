1006f174fa58e3be608e5ea1dac3816e0bd385ce
Tom Dison <fretinator@smallvue.com>
2022-09-21 18:00:16 -0500

    Update BoomerBox.ino
    Uploaded correct file

--- BoomerBox.ino
+++ BoomerBox.ino
@@ -1,30 +1,79 @@
-\#include <VS1053.h>
+#include <Adafruit_VS1053.h>
+
 #include <SPI.h>
-
+#include <Wire.h>
 #include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#include <WiFi.h> 
 
-
-#include <WiFi.h>
-#define VS1053_CS     5
-#define VS1053_DCS    16
-#define VS1053_DREQ   4
 
 // Default volume
-#define DEFAULT_VOLUME  80
+#define DEFAULT_VOLUME  100
+
+// OLOED screen
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
 
 
-// LCD constants
-char ESC = 0xFE; // Used to issue command
-const char CLS = 0x51;
-const char CURSOR = 0x45;
-const char LINE1_POS = 0x00;
-const char LINE2_POS = 0x40;
-const char LINE3_POS = 0x14;
-const char LINE4_POS = 0x54;
-const String NOT_FOUND = "n/a";
+#define VS_MOSI 35
+#define VS_MISO 37
+#define VS_SCK 36
+// Feather ESP8266
+#if defined(ESP8266)
+  #define VS1053_CS      16     // VS1053 chip select pin (output)
+  #define VS1053_DCS     15     // VS1053 Data/command select pin (output)
+  #define CARDCS          2     // Card chip select pin
+  #define VS1053_DREQ     0     // VS1053 Data request, ideally an Interrupt pin
 
-VS1053 player(VS1053_CS, VS1053_DCS, VS1053_DREQ);
+// Feather ESP32
+#elif defined(ESP32) && !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+  #define VS1053_CS      32     // VS1053 chip select pin (output)
+  #define VS1053_DCS     33     // VS1053 Data/command select pin (output)
+  #define CARDCS         14     // Card chip select pin
+  #define VS1053_DREQ    15     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather Teensy3
+#elif defined(TEENSYDUINO)
+  #define VS1053_CS       3     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          8     // Card chip select pin
+  #define VS1053_DREQ     4     // VS1053 Data request, ideally an Interrupt pin
+
+// WICED feather
+#elif defined(ARDUINO_STM32_FEATHER)
+  #define VS1053_CS       PC7     // VS1053 chip select pin (output)
+  #define VS1053_DCS      PB4     // VS1053 Data/command select pin (output)
+  #define CARDCS          PC5     // Card chip select pin
+  #define VS1053_DREQ     PA15    // VS1053 Data request, ideally an Interrupt pin
+
+#elif defined(ARDUINO_NRF52832_FEATHER )
+  #define VS1053_CS       30     // VS1053 chip select pin (output)
+  #define VS1053_DCS      11     // VS1053 Data/command select pin (output)
+  #define CARDCS          27     // Card chip select pin
+  #define VS1053_DREQ     31     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather M4, M0, 328, ESP32S2, nRF52840 or 32u4
+#else
+  #define VS1053_CS       6     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          5     // Card chip select pin
+  // DREQ should be an Int pin *if possible* (not possible on 32u4)
+  #define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+
+#endif
+
+
+//Adafruit_VS1053 player(VS_MOSI, VS_MISO,  VS_SCK, 
+// VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ);
+
+  Adafruit_VS1053 player(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ);
+
+
 WiFiClient client;
 
 // WiFi settings example, substitute your own
@@ -32,14 +81,13 @@ const char *ssid = "MyCharterWiFi49-2G";
 const char *password = "LazarusRises971";
 
 // Button
-//const int buttonPin = 25;     // the number of the pushbutton pin
-//int buttonState = 0;         // variable for reading the pushbutton status
-//int lastState = LOW;
-
+const int buttonPin = 25;     // the number of the pushbutton pin
+int buttonState = 0;         // variable for reading the pushbutton status
+int lastState = LOW;
 
 // The buffer size 64 seems to be optimal. At 32 and 128 the sound might be brassy.
-const int BUFFER_SIZE = 64;
-uint8_t mp3buff[BUFFER_SIZE];
+const int BUFFER_SIZE = 32;
+PROGMEM uint8_t mp3buff[BUFFER_SIZE];
 
 // *** VOLUME  ***
 const int VOLUME_KNOB_INPUT = 34; // Must use ADC1, NOT ADC2 when using WiFi on ESP32
@@ -49,105 +97,59 @@ int SOUND_CALIBRATE_INTERVAL = 500;
 // Check volume 20 times to get high and low values
 int NUM_SOUND_CALIBRATIONS = 20;
 // For my esp32 dev kit, verified using one-time calibrateSound()
-int knobHigh = 4095;
-int knobLow = 0; 
-int lastVolumeKnobInput;nput;
-const int KNOB_THRESHOLD = 100; // Must change by 100 before changing volume
-const int CHECK_MILLIS = 1000; // Check if volume has changed every 1 second, be patient!
-const int SAMPLE_CHECKS = 10; // Don't rely on a single reading
-const int SAMPLE_PAUSE = 10; // Small delay between measuring
+int soundHigh = 4095;
+int soundLow = 0; 
+int lastVolumeKnobInput;
+const int VOL_KNOB_THRESHOLD = 100; // Must change by 100 before changing volume
+const int VOL_CHECK_MILLIS = 1000; // Check if volume has changed every 1 second, be patient!
+const int VOL_SAMPLE_CHECKS = 10; // Don't rely on a single reading
+const int VOL_SAMPLE_PAUSE = 10; // Small delay between measuring
 int lastVolCheckMillis = 0;
-int lastStationCheckMillis = 0;
 bool firstRun = true;
 // Some stations
 const int NUM_STATIONS = 9;
-//const char VOL_CHAR = '*';
+const char VOL_CHAR = '*';
 
 // These are stations that were working for me in January 2020 - Tom Dison
-const char *station[NUM_STATIONS] = {"UK Oldies", "Magic Old", "1FM Blues", "SomaFM", "Hotmix Rk",
+const char *station[NUM_STATIONS] = {"AnonRadio", "Magic Old", "1FM Blues", "SomaFM", "Hotmix Rk",
   "Blues Cove","Rad Volta", "Lf Praise","DJ Trance"};
-const char *host[NUM_STATIONS] = {"149.255.59.162","airspectrum.cdnstream1.com","sc2b-sjc.1.fm", "ice1.somafm.com", 
+const char *host[NUM_STATIONS] = {"anonradio.net","airspectrum.cdnstream1.com","sc2b-sjc.1.fm", "ice1.somafm.com", 
   "streamingads.hotmixradio.fm", "radio.streemlion.com","air.radiolla.com","radio.cgbc.org",
   "globaldjbroadcast.cc"};
-const char *path[NUM_STATIONS] = {"/1","/1261_192","/","/u80s-128-mp3", "/hotmixradio-rock-128.mp3",
+const char *path[NUM_STATIONS] = {"/anonradio","/1261_192","/","/u80s-128-mp3", "/hotmixradio-rock-128.mp3",
   "/stream","/volta.32k.mp3","/3.mp3","/world96k"};
-int   port[NUM_STATIONS] = {8062,8000,8030,80,80,2070,80,8000,8000};
-int curStation = 0;
+int   port[NUM_STATIONS] = {8000,8000,8030,80,80,2070,80,8000,8000};
+int curStation = 1;
 int curVolume = DEFAULT_VOLUME;
 int curDisplayVolume = DEFAULT_VOLUME;
-const int SCREEN_ROWS = 4;
-const int SCREEN_COLS = 20;
-const int SCREEN_CHARS = 80;
 
-char getLinePos(int line_num) {
-  switch(line_num) {
-    case 1:
-      return LINE1_POS;
-    case 2:
-      return LINE2_POS;
-    case 3:
-      return LINE3_POS;
-    case 4:
-      return LINE4_POS;
-    default:
-      return LINE1_POS; // Default to line 1
-  }
-}
-void clearScreen() {
-  Serial2.write(ESC);
-  Serial2.write(CLS);  
-  delay(10);
-}
-
-// Lines are 1 based
-void printScreen(const char* line, bool cls = true, int whichLine = 1) {
-  // Clear screen
-  if(cls) {
-    clearScreen();
+const char* getVolumeChars() {
+  String ret = "";
+  int numChars = curDisplayVolume / 8;
+  for(int i = 0; i < numChars; i++) {
+    ret += VOL_CHAR;
   }
 
-  Serial2.write(ESC);
-  Serial2.write(CURSOR);
-  Serial2.write(getLinePos(whichLine));
-  delay(10);
-  Serial2.print(line);
+  return ret.c_str();
 }
+
 
 void setupScreen() {
   // Initialize serial
   Serial.println("Setting up screen.");
   
-  // Setup LCD
-    // Initialize LCD module
-  Serial2.write(ESC);
-  Serial2.write(0x41);
-  Serial2.write(ESC);
-  Serial2.write(0x51);
-  
-  // Set Contrast
-  Serial2.write(ESC);
-  Serial2.write(0x52);
-  Serial2.write(40);
-  
-  // Set Backlight
-  Serial2.write(ESC);
-  Serial2.write(0x53);
-  Serial2.write(8);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 
-  Serial2.write(ESC);
-  Serial2.write(CLS);
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+ // Clear the buffer
+  display.clearDisplay();
   
-  Serial2.print("NKC Electronics");
-  
-  // Set cursor line 2, column 0
-  Serial1.write(ESC);
-  Serial1.write(CURSOR);
-  Serial1.write(LINE2_POS);
-  
-  Serial2.print("20x4 Serial LCD");
-  
-  delay(1000);
-
 }
 
 // Only used to verify high and low values
@@ -159,19 +161,19 @@ void calibrateSound() {
 
     Serial.println("Cal readVal = " + String(readVal));
   
-    if(readVal > knobHigh) {
-      knobHigh = readVal;
+    if(readVal > soundHigh) {
+      soundHigh = readVal;
     }
 
-    if(readVal < knobLow) {
-      knobLow = readVal;
+    if(readVal < soundLow) {
+      soundLow = readVal;
     }
 
     delay(SOUND_CALIBRATE_INTERVAL);
   }
 
-  Serial.println("Sound high: " + String(knobHigh));
-  Serial.println("Sound low: " + String(knobLow));
+  Serial.println("Sound high: " + String(soundHigh));
+  Serial.println("Sound low: " + String(soundLow));
   lastVolumeKnobInput = readVal;
 }
 
@@ -208,16 +210,29 @@ int calculateSoundVolume() {
   return int(calcVolume);
 }
 
-
-
 void updateScreen() {
-  printScreen("#" + String(curStation) + " - " + String(station[curStation]),
-  "Volume:"), String(curDisplayVolume).c_str(), null);
+  display.clearDisplay();
+  //
+
+  Serial.print("Station:");
+  Serial.println(station[curStation]);
+  // Print stationstation[curStation], 
+  display.setTextSize(2); 
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor (1,1);
+  display.println(station[curStation]);
+  //display.println("");
+  
+  //display.setTextSize(1);
+  // Draw volume bar
+  int barLength = (curDisplayVolume / 100.0) * display.width();
+  display.fillRect(0, 20, barLength, 12, SSD1306_WHITE);
+  display.display();
 }
 
 void connectToStation() {
   Serial.print("connecting to station: ");
-  Serial.println("#" + String(curStation) + " - " + String(station[curStation]));
+  Serial.println(station[curStation]);
 
   if (!client.connect(host[curStation], port[curStation])) {
       Serial.println("Connection failed");
@@ -234,12 +249,24 @@ void connectToStation() {
   updateScreen();
 }
 
+void onButtonClick() {
+  curStation++;
+
+  if(NUM_STATIONS == curStation) {
+    curStation = 0;
+  }
+
+  connectToStation();
+}
+
 void setup() {
     Serial.begin(115200);
     //calibrateSound();
-    setupScreen();
+
     pinMode(VOLUME_KNOB_INPUT, INPUT_PULLUP);
-    //pinMode(STATION_KNOB_INPUT, INPUT_PULLUP);  
+  
+    pinMode(buttonPin, INPUT);
+    lastState = digitalRead(buttonPin);    
     // Wait for VS1053 and PAM8403 to power up
     // otherwise the system might not start up correctly
     delay(1000);
@@ -249,24 +276,23 @@ void setup() {
 
     Serial.println("\n\nSimple Radio Node WiFi Radio");
 
+    setupScreen();
+
     SPI.begin();
-    delay(1000); // For MP3 Player
+
     Serial.println("SPI Started");
     player.begin();
 
     Serial.println("Player started");
     
-    player.switchToMp3Mode();
-
-    Serial.println("Switched to MP3 Mode");
 
     if(!soundCalibrated) {
       //calibrateSound();
     }
 
-    curVolume = calculateSoundVolume();
+    curVolume = DEFAULT_VOLUME; //                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                calculateSoundVolume();
     Serial.println("Calculated volume = " + String(curVolume) + "%");
-    player.setVolume(curVolume);
+    player.setVolume(100 - curVolume, 100 - curVolume);
 
     Serial.println("Volume is set");
 
@@ -283,7 +309,6 @@ void setup() {
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 
-    curStation = calculateStation();
     connectToStation();
 }
 
@@ -291,22 +316,22 @@ void checkSound() {
   int curMillis = millis();
 
   if(curMillis < lastVolCheckMillis // millis rolled over
-      || (curMillis - lastVolCheckMillis) > CHECK_MILLIS) {
+      || (curMillis - lastVolCheckMillis) > VOL_CHECK_MILLIS) {
 
     int totalSampled = 0;
-    for(int v = 0; v < SAMPLE_CHECKS; v++) {
+    for(int v = 0; v < VOL_SAMPLE_CHECKS; v++) {
       totalSampled += analogRead(VOLUME_KNOB_INPUT);
-      delay(SAMPLE_PAUSE);
+      delay(VOL_SAMPLE_PAUSE);
     }
 
-    int curVol = totalSampled / SAMPLE_CHECKS;
+    int curVol = totalSampled / VOL_SAMPLE_CHECKS;
     
     lastVolCheckMillis = curMillis;
     
-    if(abs(curVol - lastVolumeKnobInput) >= KNOB_THRESHOLD) {
+    if(abs(curVol - lastVolumeKnobInput) >= VOL_KNOB_THRESHOLD) {
       curVolume = calculateSoundVolume();
       Serial.println("Calculated volume = " + String(curVolume) + "%");
-      player.setVolume(curVolume);
+      player.setVolume(100 - curVolume, 100 - curVolume);
       updateScreen();
       lastVolumeKnobInput = curVol;
     }
@@ -315,19 +340,34 @@ void checkSound() {
 
 void loop() {
   if(!firstRun) {
-    checkSound();
-    //checkStation();
+    //checkSound();
   } else {
     firstRun = false;
   }
+
+   // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    if(lastState == LOW) {
+      Serial.print("Changing station");
+      onButtonClick();
+      delay(100); // for bounce of button
+    }
+  } 
+
+  lastState = buttonState;
   
   if (!client.connected()) {
     connectToStation();
   }
 
-  if (client.available() > 0) {
-      // The buffer size 64 seems to be optimal. At 32 and 128 the sound might be brassy.
-      uint8_t bytesread = client.read(mp3buff, BUFFER_SIZE);
-      player.playChunk(mp3buff, bytesread);
+  if(player.readyForData()) {
+    if (client.available() > 0) {
+        // The buffer size 64 seems to be optimal. At 32 and 128 the sound might be brassy.
+        uint8_t bytesread = client.read(mp3buff, BUFFER_SIZE);
+        player.playData(mp3buff ,bytesread);
+    }
   }
 }
